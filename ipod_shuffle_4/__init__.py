7532584c0f847a8b5ca91b396f25a4a61f2c039f
Observer Chan <observerchan@gmail.com>
2016-01-15 23:30:01 +0800

    none

--- ipod_shuffle_4/__init__.py
+++
@@ -1,9 +0,0 @@
-from .track import Track
-from .playlist import Playlist
-from .shuffle import Shuffle, format_dbid
-
-from . import fields, extensions, utils
-
-
-
-__all__ = ['Shuffle','Track', 'Playlist', 'fields', 'extensions', 'utils']

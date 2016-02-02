83a11054238ea5fbc7e18284f7808bced6381da8
Chen Meng <observerchan@gmail.com>
2016-02-02 16:59:36 +0800

    none

--- test.py
+++ test.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import pprint
+
 import shuffle.itunessd
 
 itunessd = open('iTunesSD', 'rb').read()
 
 a, b, c = shuffle.itunessd.itunessd_to_dics(itunessd)
 
-print(a)
-print(b)
-print(c)
+pp = pprint.PrettyPrinter(indent=4)
+# pa = pprint.pprint(a)
+# pb = pprint.pprint(b)
+# pc = pprint.pprint(c)
+
+print(pp.pprint(a), end='\n\n\n')
+print(pp.pprint(b), end='\n\n\n')
+print(pp.pprint(c), end='\n\n\n')

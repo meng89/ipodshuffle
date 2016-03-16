f0587df8b095bc98909a0f91eed7db1f57350e0f
Chen Meng <observerchan@gmail.com>
2016-03-16 18:22:50 +0800

    none

--- teresa/__init__.py
+++ teresa/__init__.py
@@ -6,10 +6,11 @@ _usr_share_locale = sys.prefix + '/share/locale'
 _locale_dirs = gettext.find('teresa', _usr_share_locale)
 
 print(locale.getlocale()[0])
+language = 'C'
 
 _locale_lang = gettext.translation("teresa",
                                    _usr_share_locale if _locale_dirs else 'ipodshuffle/tools/locale',
-                                   languages=[locale.getlocale()[0]], fallback=True)
+                                   languages=[language], fallback=True)
 _locale_lang.install(True)
 
 translate = _locale_lang.gettext

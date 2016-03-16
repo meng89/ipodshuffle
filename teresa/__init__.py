dbc7e35a304333bd437443713537685514d4f9a4
Observer Chan <observerchan@gmail.com>
2016-03-16 19:44:12 +0800

    fix error when locale is C

--- teresa/__init__.py
+++ teresa/__init__.py
@@ -2,12 +2,17 @@ import gettext
 import sys
 import locale
 
+
 _usr_share_locale = sys.prefix + '/share/locale'
+
 _locale_dirs = gettext.find('teresa', _usr_share_locale)
 
+
 _locale_lang = gettext.translation("teresa",
-                                   _usr_share_locale if _locale_dirs else 'ipodshuffle/tools/locale',
-                                   languages=[locale.getlocale()[0]], fallback=True)
+                                   _usr_share_locale if _locale_dirs else 'teresa/locale',
+                                   languages=[locale.getlocale()[0] or 'en_US'],
+                                   fallback=True)
+
 _locale_lang.install(True)
 
 translate = _locale_lang.gettext
